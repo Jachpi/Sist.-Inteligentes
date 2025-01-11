4bfd839eae8cea225c5538c7152b45467675177a
Jachpi <121670083+Jachpi@users.noreply.github.com>
2025-01-11 18:50:17 +0100

    .

--- Dataset/cambiar_formato_a_dict.py
+++ Dataset/cambiar_formato_a_dict.py
@@ -17,7 +17,8 @@ def remove_common_keys_from_dict1(nand_op_film, film_vector):
     common_keys = nand_op_film.copy()
     for key in common_keys.keys():
         nand_op_film.pop(key)
-        film_vector.pop(key)
+        if key in film_vector.keys():
+            film_vector.pop(key)
 
 
 
@@ -25,12 +26,11 @@ def remove_common_keys_from_dict1(nand_op_film, film_vector):
 with (open(
         'C:\\Users\JAVI\PycharmProjects\Sistemas-Int\Sist.-Inteligentes\Dataset\TF-IDF.json') as tf_idf):
     vectors = json.load(tf_idf)
-    current_film_vector = vectors['23']
-
     similarities = []
 
     for id, other_film_vector in vectors.items():
-        if (id == '23'):
+        current_film_vector = vectors['23'].copy()
+        if (id != '23'):
             curr_film_array = []
             other_film_array = []
 
@@ -50,7 +50,7 @@ with (open(
             #eliminación de las palabras usadas
             remove_common_keys_from_dict1(nand_op_curr_film, current_film_vector)
             remove_common_keys_from_dict1(nand_op_other_film, other_film_vector)
-            print("oth: "+str(other_film_vector))
+
             for word, val in other_film_vector.items():
                 if word in current_film_vector:
                     curr_film_array.append(current_film_vector[word])
@@ -60,15 +60,13 @@ with (open(
                 other_film_array.append(val)
                 nand_op_other_film[word] = -999
 
-            print("curr: "+current_film_vector)
-            print("other: "+other_film_vector)
 
 
 
             sim = 1-cosine(curr_film_array, other_film_array)
             similarities.append((id, sim))
 
-    #similarities.sort(key=lambda cos_sim: cos_sim[1])
+    similarities.sort(reverse= True, key=lambda cos_sim: cos_sim[1])
 
     print(similarities)
 
